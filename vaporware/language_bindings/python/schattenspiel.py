f350a3cecbc8e726c631f98f0d259ad8ae28811d
Felix Kaiser <felix.kaiser@fxkr.net>
2013-06-09 15:27:42 +0200

    Small bugfix for one of the demo programs

--- vaporware/language_bindings/python/schattenspiel.py
+++ vaporware/language_bindings/python/schattenspiel.py
@@ -6,8 +6,11 @@ import login
 
 dev = login.connect()
 last_j = 0
+for i in xrange(20):
+    dev.set_rgba(i, (0, 0, 0, 255))
+max_led = 20
 for i in itertools.count():
-    j = int(math.sin(i / 15.) * 7.5 + 7.5)
+    j = int(math.sin(i / float(max_led)) * float(max_led)/2 + float(max_led)/2)
     dev.set_rgb(last_j, (0, 0, 0))
     dev.set_rgb(j, (255, 255, 255))
     last_j = j
