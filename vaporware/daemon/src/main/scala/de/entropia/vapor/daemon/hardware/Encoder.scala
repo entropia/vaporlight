b25fd0c0846c0423f609116f07e6b3275f18d4fb
Felix Kaiser <felix.kaiser@fxkr.net>
2013-09-22 23:17:36 +0200

    Daemon: don't double strobe
    Turns out double strobe is not actually necessary (anymore?)

--- vaporware/daemon/src/main/scala/de/entropia/vapor/daemon/hardware/Encoder.scala
+++ vaporware/daemon/src/main/scala/de/entropia/vapor/daemon/hardware/Encoder.scala
@@ -11,11 +11,6 @@ class Encoder(val framer: Framer) {
   }
 
   def strobe() {
-    // always send *two* strobes - a workaround for a problem with the bus protocol:
-    // because of how framing works, the Vaporlight firmware can only detect the end
-    // of a frame by waiting for the beginning of the next frame. This is problematic
-    // if only a single strobe without further messages is sent.
-    framer.write(Vector(0xfe.toByte))
     framer.write(Vector(0xfe.toByte))
     framer.flush()
   }
