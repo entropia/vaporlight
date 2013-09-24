cbfbb8078d74bd488de0a0e5818ed6457c8a6da5
Felix Kaiser <felix.kaiser@fxkr.net>
2013-09-24 15:20:44 +0200

    Daemon: use new bus protocol
    This is just a quick fix; we don't make use of the
    new features yet:
    
    - we set raw pwm values instead of xyY colors
    - we use only 8 bit precision instead of 16

--- vaporware/daemon/src/main/scala/de/entropia/vapor/daemon/hardware/Encoder.scala
+++ vaporware/daemon/src/main/scala/de/entropia/vapor/daemon/hardware/Encoder.scala
@@ -5,9 +5,11 @@ package de.entropia.vapor.hardware
  * Translates commands into byte sequences.
  */
 class Encoder(val framer: Framer) {
+  val SET_RAW_COMMAND = 0x00.toByte
+  val SET_XYY_COMMAND = 0x01.toByte
 
   def update(module: Byte, values: Seq[Byte]) {
-    framer.write(Vector(module) ++ values)
+    framer.write(Vector(module, SET_RAW_COMMAND) ++ values.flatMap(v => Vector(v, v)))
   }
 
   def strobe() {
