d8bb74371e1483f72c33d16793edbce37c043e31
Felix Kaiser <felix.kaiser@fxkr.net>
2013-09-29 18:53:58 +0200

    Make 'fade.py' use high resolution commands

--- vaporware/language_bindings/python/fade.py
+++ vaporware/language_bindings/python/fade.py
@@ -3,17 +3,19 @@ import llvp
 
 
 def main(light, num_leds):
+    # we only use 1/10th of the possible resolution
     while True:
-        for intensity in xrange(255): # fade up
+        for intensity in xrange(0, 65535, 10): # fade up
             apply(light, num_leds, intensity)
-        for intensity in xrange(255, -1, -1): # fade down
+            print(intensity)
+        for intensity in xrange(65535, -1, -10): # fade down
             apply(light, num_leds, intensity)
 
 def apply(light, num_leds, intensity):
     for led in xrange(num_leds):
-        light.set_rgb(led, (intensity, intensity, intensity))
+        light.set_rgb_hi(led, (intensity, intensity, intensity))
     light.strobe()
-    time.sleep(0.02)
+    time.sleep(0.001)
 
 
 if __name__ == "__main__":
