39b65057f6bf09f235fd00f44bf4e826ed151cf6
Florian Larysch <fl@n621.de>
2014-02-17 17:17:58 +0100

    util: update version-color.py to match the new color output

--- vaporware/util/version-color.py
+++ vaporware/util/version-color.py
@@ -13,23 +13,23 @@ import sys
 
 rev = "HEAD" if len(sys.argv) < 2 else sys.argv[1]
 sha1 = subprocess.check_output(["git", "rev-parse", rev]).strip()
-num = int(sha1[-6:], 16)
+num = int(sha1[0:4], 16)
 
-b0_r = (num & (1<<0)) != 0
-b0_g = (num & (1<<1)) != 0
-b0_b = (num & (1<<2)) != 0
-b1_r = (num & (1<<3)) != 0
-b1_g = (num & (1<<4)) != 0
-b1_b = (num & (1<<5)) != 0
-b2_r = (num & (1<<6)) != 0
-b2_g = (num & (1<<7)) != 0
-b2_b = (num & (1<<8)) != 0
-b3_r = (num & (1<<9)) != 0
-b3_g = (num & (1<<10)) != 0
-b3_b = (num & (1<<11)) != 0
-b4_r = (num & (1<<12)) != 0
-b4_g = (num & (1<<13)) != 0
-b4_b = (num & (1<<14)) != 0
+b0_r = (num & (1<<15)) != 0
+b0_g = (num & (1<<14)) != 0
+b0_b = (num & (1<<13)) != 0
+b1_r = (num & (1<<12)) != 0
+b1_g = (num & (1<<11)) != 0
+b1_b = (num & (1<<10)) != 0
+b2_r = (num & (1<<9)) != 0
+b2_g = (num & (1<<8)) != 0
+b2_b = (num & (1<<7)) != 0
+b3_r = (num & (1<<6)) != 0
+b3_g = (num & (1<<5)) != 0
+b3_b = (num & (1<<4)) != 0
+b4_r = (num & (1<<3)) != 0
+b4_g = (num & (1<<2)) != 0
+b4_b = (num & (1<<1)) != 0
 
 colors = {
     (0,0,0): "black",
@@ -43,8 +43,8 @@ colors = {
 }
 
 print "git commit: %s" % sha1
-print "=> [%s]_10" % (num & 0b111111111111111)
-print "=> [%s]_2" % bin(num & 0b111111111111111)[2:]
+print "=> [%s]_10" % (num & 0b1111111111111110)
+print "=> [%s]_2" % bin(num & 0b1111111111111110)[2:]
 print
 
 print "%i%i%i => %s" % (b0_r, b0_g, b0_b, colors[b0_r, b0_g, b0_b])
